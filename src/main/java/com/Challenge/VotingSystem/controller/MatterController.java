ce50f9391ee936865b99963c058565720b942c17
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-15 22:57:01 -0300

    feat: controller
    Initialized controller package by creating the MatterController. Should implement other controllers on the next commit/push

---
+++ src/main/java/com/Challenge/VotingSystem/controller/MatterController.java
@@ -0,0 +1,55 @@
+package com.Challenge.VotingSystem.controller;
+
+import com.Challenge.VotingSystem.entity.Matter;
+import com.Challenge.VotingSystem.service.MatterService.MatterService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/matters")
+public class MatterController {
+//        @Autowired
+//    MatterService service;
+    private final MatterService service;
+
+    public MatterController(MatterService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Matter>> getMatters() {
+        List<Matter> matters = service.findAll();
+        return ResponseEntity.ok().body(matters);
+    }
+
+    @GetMapping(value = "matter/{id}")
+    public ResponseEntity<Matter> findMatterById(@PathVariable("id") Long id) {
+        Matter matter = service.findById(id);
+        return ResponseEntity.ok(matter);
+    }
+
+    @PostMapping
+    public ResponseEntity<Matter> save(@RequestBody Matter matter) {
+        Matter created = service.save(matter);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Matter> delete(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    // create voter, session, matter
+
+
+}
