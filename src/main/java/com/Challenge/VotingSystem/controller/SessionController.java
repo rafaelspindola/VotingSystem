1725e1b30608a2a4f2c1cbaa2b7a92ec1860717e
Rafael Spíndola <rafa.spin95@gmail.com>
2023-01-16 21:33:06 -0300

    feat: Session Controller
    Created SessionControler

---
+++ src/main/java/com/Challenge/VotingSystem/controller/SessionController.java
@@ -0,0 +1,49 @@
+package com.Challenge.VotingSystem.controller;
+
+import com.Challenge.VotingSystem.entity.Session;
+import com.Challenge.VotingSystem.service.SessionService.SessionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/sessions")
+public class SessionController {
+
+    private final SessionService service;
+
+    public SessionController(SessionService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Session>> getSessions() {
+        List<Session> sessions = service.findAll();
+        return ResponseEntity.ok().body(sessions);
+    }
+
+    @GetMapping(value = "session/{id}")
+    public ResponseEntity<Session> findSessionById(@PathVariable("id") Long id) {
+        Session session = service.findById(id);
+        return ResponseEntity.ok(session);
+    }
+
+    @PostMapping
+    public ResponseEntity<Session> save(@RequestBody Session session) {
+        Session created = service.save(session);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().
+                path("{id}").
+                buildAndExpand(created.getId()).
+                toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Session> deleteSession(@PathVariable("id") Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
